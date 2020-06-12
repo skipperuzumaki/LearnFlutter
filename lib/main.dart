b400750875c2b44deecd487afbe96b8100a2ec81
MD Ruhulamin Khan <skipperuzumaki@gmail.com>
2020-06-13 00:35:02 +0530

    static wigits and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Hello World"),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text("Hey People")
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text("Click"),
-    ),
-  ),
+  home: Home()
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Hello World",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreenAccent,
+      ),
+      body: Center(
+          child: Image(
+            image: AssetImage('Assets/space.jpg'),
+          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+          "Click",
+          style: TextStyle(
+              color: Colors.black
+          ),
+        ),
+        backgroundColor: Colors.lightGreenAccent,
+      ),
+    );
+  }
+}
